8d7d96ce309b2eb6355276a57ec80776931b3aea
dejwcpp <dejwcpp@gmail.com>
2024-03-22 09:33:27 +0100

    draws circle on clicked point

--- Fastest-route-graph/MainPage.xaml.cs
+++ Fastest-route-graph/MainPage.xaml.cs
@@ -3,6 +3,17 @@ using System.Drawing;
 
 namespace Fastest_route_graph
 {
+    /* To Do:
+     * 
+     * TranslatePointToTheMiddleOfaCircle doesnt work. It worked when it wasnt seperated method tho
+     * 
+     * jak kliknieto w kolo to polacz ze srodkiem
+     * podswietlaj kolo na najechanie
+     * nie lacz z obecnym i przedostatnim nodem
+     * nie podswietlaj obecnego i przedostatniego noda
+     * 
+     */
+
     public partial class MainPage : ContentPage
     {
         private List<System.Drawing.PointF> clickedPoints = new List<System.Drawing.PointF>();
@@ -13,7 +24,7 @@ namespace Fastest_route_graph
             RightSide.SizeChanged += BoxViewSizeChanged;
         }
 
-        private void MouseLeftClick(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+        private void MouseLeftClick(Object sender, TappedEventArgs e)
         {
             // Get the position relative to the tapped UI element.
             Microsoft.Maui.Graphics.Point? point = e.GetPosition((View)sender);
@@ -22,6 +33,11 @@ namespace Fastest_route_graph
             {
                 int x = (int)point.Value.X;
                 int y = (int)point.Value.Y;
+
+                System.Drawing.PointF p = new System.Drawing.PointF(x, y);
+
+                // when clicked in existing circle connect to the middle of it
+                p = TranslatePointToTheMiddleOfaCircle(p);
 
                 // updates clickedPoints List in this class
                 clickedPoints.Add(new System.Drawing.PointF(x, y));
@@ -33,6 +49,24 @@ namespace Fastest_route_graph
                 // the Draw method is called each time when mouse left is clicked thanks to this
                 DrawSurface.Invalidate();
             }
+        }
+
+        // when clicked in existing circle connects to the middle of it
+        private System.Drawing.PointF TranslatePointToTheMiddleOfaCircle(System.Drawing.PointF p)
+        {
+            for (int i = 0; i < clickedPoints.Count; i++)
+            {
+                double distance = Math.Sqrt(Math.Pow(p.X - clickedPoints[i].X, 2) + Math.Pow(p.Y - clickedPoints[i].Y, 2));
+
+                if (distance <= 30)
+                {
+                    p.X = (int)clickedPoints[i].X;
+                    p.Y = (int)clickedPoints[i].Y;
+
+                    return p;
+                }
+            }
+            return p;
         }
 
         // sets drawable field to the area of BoxView
@@ -47,12 +81,19 @@ namespace Fastest_route_graph
     }
     public class Drawing : IDrawable
     {
+
         public List<System.Drawing.PointF> clickedPoints { get; set; } = new List<System.Drawing.PointF>();
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
             canvas.StrokeColor = Colors.White;
+            canvas.FillColor = Colors.White;
             canvas.StrokeSize = 6;
+
+            for (int i = 0; i < clickedPoints.Count; i++)
+            {
+                canvas.FillCircle(clickedPoints[i].X, clickedPoints[i].Y, 30);
+            }
 
             for (int i = 0; i < clickedPoints.Count - 1; i++)
             {
